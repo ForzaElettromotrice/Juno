0ee071e1ad866baa4fbe225fe324281cba2b37e2
R0n3l <eleron2002@gmail.com>
2022-06-07 21:09:54 +0200

    implemented exit button in main menu set stage title

--- src/main/java/org/juno/Juno.java
+++ src/main/java/org/juno/Juno.java
@@ -23,6 +23,7 @@ public class Juno extends Application
 	public void start(Stage stage) throws IOException {
 		FXMLLoader loader = new FXMLLoader(Juno.class.getResource("startMenu.fxml"));
 		Scene scene = new Scene(loader.load());
+		stage.setTitle("JUno");
 		stage.setScene(scene);
 		stage.show();
 	}
