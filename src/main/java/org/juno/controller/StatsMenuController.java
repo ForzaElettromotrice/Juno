10ebf20b91fa642a18f4910aae1075b1655e35df
R0n3l <eleron2002@gmail.com>
2022-06-12 13:38:25 +0200

    TODO: fix backgroundStats

--- src/main/java/org/juno/controller/StatsMenuController.java
+++ src/main/java/org/juno/controller/StatsMenuController.java
@@ -1,6 +1,7 @@
 package org.juno.controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
 import org.juno.view.NonexistingSceneException;
 import org.juno.view.StatsMenuView;
 
@@ -17,5 +18,20 @@ public class StatsMenuController
     public void menuClicked() throws NonexistingSceneException
     {
         STATS_MENU_VIEW.changeScene(0);
+    }
+
+    public void changeEntered(MouseEvent mouseEvent) {
+    }
+
+    public void changeExited(MouseEvent mouseEvent) {
+    }
+
+    public void backEntered(MouseEvent mouseEvent) {
+    }
+
+    public void backExited(MouseEvent mouseEvent) {
+    }
+
+    public void backClicked(MouseEvent mouseEvent) {
     }
 }
